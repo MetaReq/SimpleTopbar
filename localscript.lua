ee5c684c52b24f46523b14ac7b7431de77f56c37
MetaReq <139707364+MetaReq@users.noreply.github.com>
2023-07-19 10:22:15 -0700

    Update localscript.lua

--- localscript.lua
+++ localscript.lua
@@ -9,10 +9,10 @@ local function da()
 end
 
 SimpleTopbar.CreateButton(
-	1,
-	a,
-	da,
-	7451945728
+	1, -- number type
+	a, -- activate function
+	da, -- deactivate function
+	7451945728 -- icon id
 )
 
 SimpleTopbar.CreateButton(
